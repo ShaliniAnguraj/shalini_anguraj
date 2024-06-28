b0ad6e866d98bb0327aebe083a9af3a1033ed3fe
SHALINI ANGURAJ <arshalini298@gmail.com>
2024-06-28 14:08:12 +0530

    commit

--- src/Components/navbar/Navbar.js
+++ src/Components/navbar/Navbar.js
@@ -19,7 +19,9 @@ const Navbar = () => {
 
   return (
     <div className="navbar">
+      <div className="nav-logo">
       <img src={logo} alt="" />
+      </div>
       <img src={menu_open} onClick={openMenu} alt="" className="nav-mob-open" />
       <ul ref={menuRef} className="nav-menu">
         <img src={menu_close} onClick={closeMenu}  alt="" className="nav-mob-close" />
